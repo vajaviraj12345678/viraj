ff24f5e381fbb1143d82ffb9dc119c709261d54f
vajaviraj12345678 <66360194+vajaviraj12345678@users.noreply.github.com>
2022-02-02 09:34:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -186,6 +186,18 @@ searchtextbox.addEventListener("input", function(){
 })
 
 
+var input = document.getElementById("addtaskinput");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("addtaskbtn").click();
+  }
+});
 
 
 
